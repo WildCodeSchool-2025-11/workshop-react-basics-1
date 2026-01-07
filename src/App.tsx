22d37476198a53070bcfe3b5a8c574bb690c2c8b
yavuzkutuk <yavuz@kutuk.fr>
2026-01-07 14:32:29 +0100

    React Basics 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import Hello from './Hello';
-import Avatar from './components/Avatar';
-import './App.css'
+import Hello from "./Hello";
+import Avatar from "./components/Avatar";
+import "./App.css";
 
 function App() {
 
   const Bart = {
-    image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+    image:
+      "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
     firstName: "Bart",
     lastName: "Simpson",
     city: "Springfield",
     age: 15,
   };
   const Homer = {
-    image: "https://www.stickees.com/files/cartoon/the-simpsons/2246-homer-simpson-donut-2.png",
+    image:
+      "https://www.stickees.com/files/cartoon/the-simpsons/2246-homer-simpson-donut-2.png",
     firstName: "Homer",
     lastName: "Simpson",
     city: "Springfield",
@@ -21,11 +23,19 @@ function App() {
 
   return (
     <>
-     <Hello />
-     <Avatar firstname={Bart.firstName} lastanme={Bart.lastName} image={Bart.image} />
-     <Avatar firstname={Homer.firstName} lastanme={Homer.lastName} image={Homer.image} />
+      <Hello />
+      <Avatar
+        firstname={Bart.firstName}
+        lastanme={Bart.lastName}
+        image={Bart.image}
+      />
+      <Avatar
+        firstname={Homer.firstName}
+        lastanme={Homer.lastName}
+        image={Homer.image}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 interface AvatarProps {
   firstname: string;
   lastanme: string;
@@ -5,10 +6,15 @@ interface AvatarProps {
 }
 
 function Avatar({ firstname, lastanme, image }: AvatarProps) {
+  const [donut, setDonut] = useState(0);
+
   return (
     <>
+      <h2>{`${firstname} ${lastanme}`}</h2>
       <img src={image} alt={`${firstname} ${lastanme}`} />
-      <p>{`Voici tel personnage ${firstname} ${lastanme}`}</p>
+      <p><img src={'/image.png'} alt={'donut'} width={64} onClick={() => setDonut(donut +1)} style={{ cursor:'pointer' }} /></p>
+      <p>{firstname} a mangé {donut} donut(s)</p>
+      <hr/>
     </>
   );
 }
